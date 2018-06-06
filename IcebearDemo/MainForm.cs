ce02d7b06f596144a5c1e7edb66f61409c146b12
Pascal Declercq <pascal.declercq@entercomputers.be>
2018-06-06 19:44:25 +0200

    Add project files.

---
+++ IcebearDemo/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IcebearDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<ReportDemo> reportDemos = new List<ReportDemo>() {
+                new ReportDemo(){Name="Customer list"}
+            };
+
+            listBox1.DataSource = reportDemos;
+            listBox1.DisplayMember = "Name";
+        }
+
+        class ReportDemo
+        {
+            public string Name { get; set; }
+            public string ReportName { get; set; }
+        }
+    }
+}
